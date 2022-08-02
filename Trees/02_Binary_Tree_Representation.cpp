332d281f8df6ba566b9a6aaa17203c251f3b3d9d
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-08-02 22:19:32 +0530

    Binary Tree Representation

--- Trees/02_Binary_Tree_Representation.cpp
+++ Trees/02_Binary_Tree_Representation.cpp
@@ -18,7 +18,7 @@ int main() {
     class Node *root = new Node(1);
 
     root -> left = new Node(2);
-    root -> left = new Node(3);
+    root -> right = new Node(3);
 
     root -> left -> right = new Node(4);
 
