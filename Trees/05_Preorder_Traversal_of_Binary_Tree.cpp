3617855498501f746ece8a2fb9900befdb8d5081
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-08-02 22:09:31 +0530

    Preorder Traversal of Binary Tree

---
+++ Trees/05_Preorder_Traversal_of_Binary_Tree.cpp
@@ -0,0 +1,40 @@
+// 144. Binary Tree Preorder Traversal
+// Link - https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+	int val;
+	class Node *left;
+	class Node *right;
+
+	Node(int data) {
+		val = data;
+		left = right = NULL;
+	}
+};
+
+void preOrder(class Node *root) {
+	if (root == NULL) return;
+
+	cout << root -> val << " ";
+
+	preOrder(root -> left);
+	preOrder(root -> right);
+}
+
+int main() {
+	class Node *root = new Node(1);
+	root -> left = new Node(2);
+	root -> left = new Node(3);
+	root -> left -> right = new Node(4);
+
+	preOrder(root);
+
+	return 0;
+}
