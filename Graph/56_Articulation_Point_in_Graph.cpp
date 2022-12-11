a1c1be3c80d0e68ef3273d1d0ee3d7ba50f95fb6
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-12-12 00:00:10 +0530

    Articulation Point

--- Graph/56_Articulation_Point_in_Graph.cpp
+++ Graph/56_Articulation_Point_in_Graph.cpp
@@ -5,9 +5,11 @@
 
 class Solution {
 public:
-	void dfs(int root, int par, int timer, vector <int> &mark, vector <int> &time, vector <int> &low, vector <int> &vis, vector<int> adj[]) {
+	int timer = 1;
+	void dfs(int root, int par, vector <int> &mark, vector <int> &time, vector <int> &low, vector <int> &vis, vector<int> adj[]) {
 		vis[root] = 1;
 		time[root] = low[root] = timer;
+		timer += 1;
 		int unVistedChild = 0;
 		for (int child : adj[root]) {
 			if (child == par) continue;
@@ -15,7 +17,7 @@ public:
 				low[root] = min(low[root], time[child]);
 				continue;
 			}
-			dfs(child, root, timer + 1, mark, time, low, vis, adj);
+			dfs(child, root, mark, time, low, vis, adj);
 			low[root] = min(low[root], low[child]);
 			if (low[child] >= time[root] && par != -1) {
 				mark[root] = 1;
@@ -32,7 +34,7 @@ public:
 		vector <int> vis(V), mark(V), time(V), low(V);
 		for (int i = 0; i < V; i++) {
 			if (vis[i]) continue;
-			dfs(i, -1, 1, mark, time, low, vis, adj);
+			dfs(i, -1, mark, time, low, vis, adj);
 		}
 
 		vector <int> ans;
