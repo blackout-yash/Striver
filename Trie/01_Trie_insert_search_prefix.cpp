275539514e4451d3d962f415f58809f946c0a255
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-12-26 18:07:40 +0530

    Implement Trie (Prefix Tree)

---
+++ Trie/01_Trie_insert_search_prefix.cpp
@@ -0,0 +1,68 @@
+// 208. Implement Trie (Prefix Tree)
+// Link - https://leetcode.com/problems/implement-trie-prefix-tree/
+
+
+
+class Node {
+public:
+	Node* links[26];
+	bool flag = false;
+
+	bool containKey(char ch) {
+		return (links[ch - 'a'] != NULL);
+	}
+
+	void put(char ch, Node *node) {
+		links[ch - 'a'] = node;
+	}
+
+	Node* get(char ch) {
+		return links[ch - 'a'];
+	}
+
+	void setEnd() {
+		flag = true;
+	}
+
+	bool isEnd() {
+		return flag;
+	}
+};
+
+class Trie {
+public:
+	Node* root;
+
+	Trie() {
+		root = new Node();
+	}
+
+	void insert(string word) {
+		Node* node = root;
+		for (auto x : word) {
+			if (node -> containKey(x) == false) {
+				node -> put(x, new Node());
+			}
+			node = node -> get(x);
+		}
+		node -> setEnd();
+	}
+
+	bool search(string word) {
+		Node* node = root;
+		for (auto x : word) {
+			if (node -> containKey(x) == false) return false;
+			node = node -> get(x);
+		}
+		return node -> isEnd();
+	}
+
+	bool startsWith(string prefix) {
+		Node* node = root;
+		for (auto x : prefix) {
+			if (node -> containKey(x) == false) return false;
+			node = node -> get(x);
+		}
+		return true;
+	}
+};
