312f91528aa2cfee5f731e3d16136820e6de9898
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-01-10 21:34:14 +0530

    Roman Number to Integer

---
+++ String/Medium/03_Roman_Number_to_Integer.cpp
@@ -0,0 +1,26 @@
+// Roman Number to Integer
+// Link - https://practice.geeksforgeeks.org/problems/roman-number-to-integer3201/1
+
+
+
+//User function template for C++
+
+class Solution {
+public:
+	int romanToDecimal(string &s) {
+		map <char, int> m;
+		m['I'] = 1;
+		m['V'] = 5;
+		m['X'] = 10;
+		m['L'] = 50;
+		m['C'] = 100;
+		m['D'] = 500;
+		m['M'] = 1000;
+		int ans = 0, n = s.size();
+		for (int i = 0; i < n; i++) {
+			ans += m[s[i]];
+			if (i && m[s[i]] > m[s[i - 1]]) ans -= (2 * m[s[i - 1]]);
+		}
+		return ans;
+	}
+};
